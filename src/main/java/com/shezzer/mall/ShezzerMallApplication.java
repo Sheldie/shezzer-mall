9307de55952a629ecc2596ee39f2dab6f8e0b52b
Shezzer <fy3225@vip.qq.com>
2020-02-24 18:04:34 +0800

    first commit

---
+++ src/main/java/com/shezzer/mall/ShezzerMallApplication.java
@@ -0,0 +1,13 @@
+package com.shezzer.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShezzerMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShezzerMallApplication.class, args);
+	}
+
+}
